edd81c1111388832a94d92502e47ccfada986c9b
unknown <cooperae@outlook.com>
2019-12-01 22:28:55 -0600

    update

---
+++ scripts.js
@@ -0,0 +1,83 @@
+/*
+
+
+<!--
+<!DOCTYPE html>
+
+<!--
+#:Uses: <p>, <header>, <footer>, <main>, <article>
+#Uses: <img>
+#uses: HTML entity... (footer &copy is one exapmle)
+	*can also use more than one git pages to make more dynamic (extra pages)
+	-->
+
+<html lang="en">
+<head>
+	<title>Alex Cooper</title>
+	<link rel="stylesheet" type = "text/css" href = "styles.css"/>
+	<script src = "scripts.js"></script>
+</head>
+
+<body>
+	<header>
+		<div class="site_header")
+			<h1>I am<br> Alex Cooper</h1>
+
+		</div>
+	</header>
+
+	<main>
+		<h2 id="introduction">Who and Why</h2>
+		<p id="intr_info">
+			paragraph on why I am great and worth hiring. certainly not just my wit and good looks. Need to find more...</p>
+
+		<h2 id = "work_hist">Work history</h2>
+		<p id = "work1_title">Quest Diagnostics</p>
+			<p id = "work1_info">
+▪ Aggregates and synthesizes data from multiple sources, utilizing conditional logic to automate recurring reports for supply distribution and warehouse logistics <br>
+▪ Leads complex issue investigations, determining the root cause of system abnormalities and quickly identifying and executing resolutions <br>
+▪ Works with the logistics team to automate and simplify current processes, reducing human error and increasing productivity <br>
+▪ Manages an efficiency initiative dedicated to modernizing and streamlining the availability of data, including creating a net new procedure to track and verify packages for the venipuncture division of the Health & Wellness business  </p>
+		<p id = "work2_title">Quest Diagnostics</p>
+			<p id = "work2_info"></p>
+		<p id = "work3_title">Ribbon Events</p>
+			<p id = "work2_info"></p>
+		<p id = "work4_title">Hanger, Inc.</p>
+			<p id = "work2_info"></p>
+			
+
+				<h2>Contact</h2>
+				<p>alexcooper@kc.rr.com --- Riverside, MO --- (816)-588-2254</p>
+
+<section>
+	<h2 id = "story">Driven by people</h2>
+	<p id = "story_p"> 
+			Soccer story Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consequat ipsum convallis nulla scelerisque venenatis. Quisque laoreet vestibulum leo et ullamcorper. Donec finibus semper ante, eu efficitur libero elementum eget. In libero diam, efficitur nec commodo sed, molestie non felis. Nulla maximus hendrerit nibh in congue. Ut vel orci finibus, dapibus mauris sit amet, fringilla ipsum. Mauris faucibus facilisis massa ac vulputate. Pellentesque in orci eu ex accumsan cursus sit amet sit amet ante. Aenean in convallis tellus. Aliquam gravida dui neque, faucibus mattis ante dictum vitae. Integer pulvinar mattis odio nec efficitur. Sed posuere ligula felis, id dictum ex malesuada nec. Nunc suscipit elit eget commodo ultrices.
+
+			Nulla posuere felis a nulla dapibus, nec scelerisque nibh convallis. In ut orci lectus. Vivamus congue leo eget tortor gravida, elementum luctus tellus rutrum. Duis lorem nibh, dapibus id turpis ac, vehicula dignissim eros. Proin eget sagittis enim. Donec sollicitudin, lacus at suscipit pellentesque, magna sapien semper elit, sed viverra magna metus nec dolor. Integer et tellus sit amet orci sagittis venenatis ac in metus. Nullam vel odio sed sapien bibendum rutrum. Vestibulum eget sodales libero. Vivamus vitae ipsum a leo viverra vulputate sed eu tellus. Maecenas vehicula fringilla lacus, quis placerat quam commodo ac. Donec tortor elit, maximus vel lacus quis, accumsan facilisis sapien. Vivamus lobortis volutpat est at placerat. Aenean vestibulum diam blandit nisl condimentum rutrum.
+	 </p>
+
+	<figure>
+		<div id = "headerBackground" style = "position: static;">
+               		
+        	</div>	
+	
+		<figcaption>
+			"Soccer Mondays. Courtesy of __________"
+		</figcaption>
+	</figure>
+</section>
+
+<!-- want a line here -->		
+
+</main>
+
+
+		<footer>&copy</footer>
+	</body>
+</html>
+
+-->
+
+
+*/
